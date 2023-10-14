f6f938ad500ad78e73fe3eb46643c0d6f4bc713a
yunjeong <stac_cato@naver.com>
2023-10-14 23:41:54 +0900

    init: 초기 설정

---
+++ new-card-game/src/main/java/piro/newcardgame/NewCardGameApplication.java
@@ -0,0 +1,13 @@
+package piro.newcardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCardGameApplication.class, args);
+	}
+
+}
---
+++ new-card-game/src/test/java/piro/newcardgame/NewCardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package piro.newcardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
