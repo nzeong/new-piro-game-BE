3d3023aef509b954e9f83527a4bd6fa5d769b566
yunjeong <stac_cato@naver.com>
2023-10-15 01:37:58 +0900

    feat: BaseEntity 추가

--- new-card-game/src/main/java/piro/newcardgame/NewCardGameApplication.java
+++ new-card-game/src/main/java/piro/newcardgame/NewCardGameApplication.java
@@ -2,7 +2,9 @@ package piro.newcardgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NewCardGameApplication {
 
