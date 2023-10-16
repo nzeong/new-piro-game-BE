589647deb85a50741d94bbfa206b85979d78b6e7
yunjeong <stac_cato@naver.com>
2023-10-16 23:32:13 +0900

    feat: Result domain 추가

---
+++ new-card-game/src/main/java/piro/newcardgame/domain/card/domain/Result.java
@@ -0,0 +1,39 @@
+package piro.newcardgame.domain.card.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import piro.newcardgame.domain.user.domain.User;
+import piro.newcardgame.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Result extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "result_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "game_id", nullable = false)
+    private Game gameId;
+
+    @Enumerated(EnumType.STRING)
+    private GameType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "winner_id", nullable = false)
+    private User winnerId;
+
+    @Builder
+    public Result(Game gameId, GameType type, User winnerId) {
+        this.gameId = gameId;
+        this.type = type;
+        this.winnerId = winnerId;
+    }
+}
